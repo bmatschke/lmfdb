c9fd8800f79802e0c5355d2e30aff6bd9dd9e643
Benjamin Matschke <benjaminmatschke@googlemail.com>
2020-08-04 05:30:24 -0400

    simplified model behind navbar.yaml
    Each heading (top button in the navbar) has a unified dropdown menu, which consists of
    - groups (such as "GL2-type, GL3-type" for modular forms), which in turn consists of
    - parts (such as Siegel modular forms, Hilbert modular forms), which can also be
    - link_bar's, which constist of titles and a bar of links (used for example for browsing L-functions by degree).
    
    A first css-styling was implemented.
    
    TODO:
    - Take care of modifications of navbar depending on beta and authentification of user.
    - Test different css-styles.
    
    	modified:   lmfdb/homepage/navbar.py
    	modified:   lmfdb/homepage/navbar.yaml
    	modified:   lmfdb/templates/navbar.html
    	modified:   lmfdb/templates/style.css

--- lmfdb/homepage/navbar.py
+++ lmfdb/homepage/navbar.py
@@ -3,33 +3,7 @@
 import os
 import yaml
 from flask import url_for
-
-def linked_name(item, level=""):
-    """ take the dictionary describing a TOC entry and return the
-    title wrapped in an appropriate href link.
-    """
-    if level == "heading":
-        return item['title']
-
-        #if 'url_for' in item:
-        #    url = url_for(item['url_for'],**item.get('url_args',dict()))
-        #    return ''.join(['<h2 class="link"><a href="',url,'">',item['title'],'</a></h2>\n'])
-        #else:
-        #    return ''.join(['<h2>',item['title'],'</h2>\n'])
-
-    else:
-        if 'url_for' in item and not ('status' in item and item['status'] == 'future'):
-            url = url_for(item['url_for'],**item.get('url_args',dict()))
-            this_entry = ''.join(['<a href="',url,'">',item['title'],'</a>'])
-        else:
-            this_entry = item['title']
-        if this_entry == 'dummy':
-            this_entry = '&nbsp;'
-        if 'status' in item and item['status'] == 'future':
-            this_entry = ''.join(['<div class="future">',this_entry,'</div>'])
-    if 'status' in item and item['status'] == 'beta':
-            this_entry = ''.join(['<div class="beta">',this_entry,'</div>'])
-    return this_entry
+from copy import deepcopy
 
 # The unique instance of the class NavBar:
 
@@ -52,33 +26,62 @@ class NavBar(object):
     def __init__(self):
         _curdir = os.path.dirname(os.path.abspath(__file__))
         self.toc_dic = yaml.load(open(os.path.join(_curdir, "navbar.yaml")), Loader=yaml.FullLoader)
+        
+        print("self.toc_dic:",self.toc_dic)
+        
         self.main_headings = list(self.toc_dic)
         self.main_headings.sort()
-        heading = lambda k: linked_name(self.toc_dic[k]['heading'],'heading')
-        self.data = [(k,heading(k),self.toc_dic[k]) for k in self.main_headings]
+ 
+        #heading = lambda k: linked_name(self.toc_dic[k]['heading'],'heading')
+        #self.data = [(k,heading(k),self.toc_dic[k]) for k in self.main_headings]
 
-        for key, head, data in self.data:
-            if data['type'] == 'L':
-                for item in data['firstpart']['entries']:
-                    item['url'] = linked_name(item)
-                for item in data['secondpart']['parts']:
-                    item['url'] = linked_name(item)
+        
+        def add_helpers_to_dropdown_dict(item):
+            # Add some (redundant) entries into the dropdown dictionary `item` (or one of its items),
+            # which make it easier to read by the template.
+            
+            #print("item:",item) #debug
+         
+            item['only_in_beta'] = 'status' in item and item['status'] == 'beta'
+            item['requires_auth_or_beta'] = 'requires' in item and item['requires'] == "auth_or_beta"
+            item['requires_not_beta'] = 'requires' in item and item['requires'] == "not_beta"
+            item['is_link'] = 'url_for' in item
+            if item['is_link']:
+                item['url'] = url_for(item['url_for'],**item.get('url_args',dict()))
+            item['is_link_bar'] = 'type' in item and item['type'] == 'link_bar'
+            
+            recurse_keys = ['groups','parts','entries']
+            for key in recurse_keys:
+                if key in item:
+                    for part in item[key]:
+                        add_helpers_to_dropdown_dict(part)
+            if 'heading' in item:
+                add_helpers_to_dropdown_dict(item['heading'])
+            
+            return        
 
-            if data['type'] == '2 column':
-                for entry in data['parts']:
-                    entry['url'] = linked_name(entry)
-                if 'part2' in entry:
-                    for pt2 in entry['part2']:
-                        pt2['url'] = linked_name(pt2)
-                        for item in pt2['parts']:
-                            item['url'] = linked_name(item)
-
-            if data['type'] in ['multilevel', 'simple']:
-                for entry in data['parts']:
-                    entry['url'] = linked_name(entry)
-                    if 'part2' in entry:
-                        for pt2 in entry['part2']:
-                            pt2['url'] = linked_name(pt2)
-                            if 'part3' in pt2:
-                                for item in pt2['part3']:
-                                    item['url'] = linked_name(item)
+        self.dropdowns = []
+      
+        for main_heading in self.main_headings:
+            parsed_dropdown = self.toc_dic[main_heading]
+            
+            #Unify the dropdown menu's syntax for easier parsing by template:
+            if 'groups' in parsed_dropdown:
+                dropdown = deepcopy(parsed_dropdown)
+            elif 'parts' in parsed_dropdown:
+                #wrap parsed_dropdown's parts into a single group of menu items:
+                dropdown = {prop: deepcopy(parsed_dropdown[prop]) for prop in parsed_dropdown if prop not in 'parts'}
+                group = {'title': "", 'parts': deepcopy(parsed_dropdown['parts'])}
+                dropdown['groups'] = [group]
+            else:
+                dropdown = deepcopy(parsed_dropdown)
+                
+            add_helpers_to_dropdown_dict(dropdown)
+            
+            self.dropdowns.append(dropdown)
+                
+            
+            print("main_heading:",main_heading)
+            print("dropdown:",dropdown)
+            
+            
